3c894250754a2e200f59ace5e843fd5524e8ef98
k-maki123 <smy9823@gmail.com>
2019-09-06 14:47:39 +0900

    list02の影の修正、ナビゲーションのスクロールを追加

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(function(){
 
+    $('header a').click(function(){
+
+        var id = $(this).attr('href');
+        var position = $(id).offset().top;
+
+        $('html,body').animate({
+            'scrollTop':position
+        },'500');
+
+    });
+
 });
